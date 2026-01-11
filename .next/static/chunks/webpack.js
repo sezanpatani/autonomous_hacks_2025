860446f01d2e69533e8f4442bba6a1ee13980f55
Keval <lkeval06@gmail.com>
2026-01-11 10:03:10 +0530

    feat: Update webpack hot module replacement files and hashes for improved development experience

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc668ab2673af912"; }
+/******/ 		__webpack_require__.h = function() { return "1ade30f9f514db20"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
